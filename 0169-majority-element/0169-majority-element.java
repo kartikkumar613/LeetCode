294e22dc367e4f4050ce66488caafa1eb208d597
kartikkumar613 <kartikkumar0495@gmail.com>
2026-08-24 11:49:33 +0530

    Time: 9 ms (39.59%), Space: 55.7 MB (46.65%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,21 +3,20 @@ class Solution {
         Arrays.sort(nums);
         int count = 1;
         int i = 1;
-        
-        while(i<nums.length){
-            if(nums[i] == nums[i-1]){
+
+        while (i < nums.length) {
+            if (nums[i] == nums[i - 1]) {
                 count++;
                 i++;
-            }
-            else{
-                count =1;
+            } else {
+                count = 1;
                 i++;
             }
-            if(count > nums.length/2){
-                return nums[i-1];
-            
-            }  
-        
+            if (count > nums.length / 2) {
+                return nums[i - 1];
+
+            }
+
         }
         return nums[0];
     }
